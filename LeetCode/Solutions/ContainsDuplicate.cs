2118297aba8e6c483d70b8e94d4f3dd9db544f02
Eric Muthomi <emuthomi@jambopay.com>
2024-02-25 12:41:25 +0300

    Contains duplicate

---
+++ LeetCode/Solutions/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Solutions
+{
+    internal class ContainsDuplicate
+    {
+        public bool ContainsDuplicates(int[] nums)
+        {
+            var map = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (map.Contains(num))
+                {
+                    return true;
+                }
+
+                map.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
